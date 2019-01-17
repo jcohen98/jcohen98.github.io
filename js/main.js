ca377e766cc5912df0877e90f0c080e74069e84a
jcohen98 <joshuacohen98@gmail.com>
2019-01-17 12:28:54 -0500

    Added portfolio page content, fixed subpage main nav bar

--- js/main.js
+++ js/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
   $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll > 200 ) {
-      $("#main-nav, #main-nav-subpage").slideDown(700);
+      $("#main-nav").slideDown(700);
       $("#main-nav-subpage").removeClass('subpage-nav');
     } else {
       $("#main-nav").slideUp(700);
